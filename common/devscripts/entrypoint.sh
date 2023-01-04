2ebcbc1a95f842197e6b460bb7ae1d6fff02a0f3
Naviator Dev <naviator.dev@gmail.com>
2023-01-04 07:54:47 +0100

    Initialize home in main container

--- common/devscripts/entrypoint.sh
+++ common/devscripts/entrypoint.sh
@@ -2,11 +2,33 @@
 
 set -eux
 
-ENVFILE="/data/.env"
-
-env | grep -vE "^HOME=|^UID=" > "${ENVFILE}"
-
 DEV_UID=$(cat /tmp/.runas | cut -d':' -f1)
+DEV_GID=$(cat /tmp/.runas | cut -d':' -f2)
+
+# Restore data
+
+echo "Restoring default volumes"
+DEFAULT_VOLUME="/default-volume"
+if [ -d ${DEFAULT_VOLUME} ]; then
+    for ENTRY in $(ls -A ${DEFAULT_VOLUME}); do
+        TARGET_PATH="/${ENTRY}"
+        if [ -z "$(ls -A ${TARGET_PATH})" ]; then
+            echo "Constructing default ${TARGET_PATH}"
+            cp -R ${DEFAULT_VOLUME}/${ENTRY} ${TARGET_PATH}
+            if [ "${DEV_UID}" -ne $(id -u) ]; then
+                chown -R ${DEV_UID}:${DEV_GID} ${TARGET_PATH}
+            fi
+        else
+            echo "${TARGET_PATH} not empty, skipping..."
+        fi
+    done
+fi
+
+# Run
+
+ENVFILE="/data/.env"
+# owner does not matter, just has to be readable
+env | grep -vE "^HOME=|^UID=" > "${ENVFILE}"
 
 if [ "${DEV_UID}" -eq $(id -u) ]; then
     tail -f /dev/null &
