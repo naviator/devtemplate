146165f6f37c406dee9b0fdfd30aee5f719995fe
Naviator Dev <naviator.dev@gmail.com>
2023-01-04 07:06:38 +0100

    Generalize ssh config

--- test/connect.sh
+++ test/connect.sh
@@ -3,7 +3,7 @@
 kubectl apply -k common
 kubectl wait --for=condition=available --timeout=60s deployment bastion
 EXPECTED=$(date)
-ssh -F .ssh/config bastion-host "echo -n \"${EXPECTED}\" > /tmp/somedata"
+ssh -F .ssh/config bastion-devtemplate "echo -n \"${EXPECTED}\" > /tmp/somedata"
 FOUND=$(kubectl exec deployment/bastion -- sh -c "cat /tmp/somedata")
 
 if [ "$EXPECTED" != "$FOUND" ]; then
@@ -14,7 +14,7 @@ fi
 kubectl apply -k develop
 sleep 1
 kubectl wait --for=condition=available --timeout=60s deployment develop
-ssh -F .ssh/config develop.default "echo -n \"${EXPECTED}\" > /tmp/somedata"
+ssh -F .ssh/config develop.default.svc "echo -n \"${EXPECTED}\" > /tmp/somedata"
 FOUND=$(kubectl exec deployment/develop -c main -- sh -c "cat /tmp/somedata")
 
 if [ "$EXPECTED" != "$FOUND" ]; then
